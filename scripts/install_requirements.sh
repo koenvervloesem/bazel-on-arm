a77f25d49e6dc77016c76501b39cd2f2c2d8c541
Koen Vervloesem <koen@vervloesem.eu>
2020-07-14 10:00:23 +0100

    Install libatomic1 as a requirement

--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -2,7 +2,7 @@
 # Install requirements to build Bazel on Raspbian Buster
 # Note: run this script with sudo
 apt update
-apt install build-essential zip unzip
+apt install build-essential zip unzip libatomic1
 apt install openjdk-11-jdk-headless # Warning: installs 1 GB of packages!
 
 # Change default JDK to OpenJDK 11
